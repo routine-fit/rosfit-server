a32b167faabaa3a9d82a4aca986856aa1531c894
Franco Marini <franco-marini@outlook.com>
2024-09-25 09:47:27 -0300

    RF-XX: filter exercises

--- src/routes/exercise/controllers.ts
+++ src/routes/exercise/controllers.ts
@@ -12,8 +12,8 @@ const getAllExercises = async (req: Request, res: Response) => {
   const exercises = await prisma.exercise.findMany({
     select: exerciseSelect,
     where: {
-      ...query,
       userId: req.firebaseUid,
+      name: { contains: query.name as string },
     },
     orderBy,
   });
